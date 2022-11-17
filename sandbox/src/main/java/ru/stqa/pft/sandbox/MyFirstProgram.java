bee83b81449b58d107b8563af281af266e28e9c1
masyannya <masyannya@mail.ru>
2022-11-17 18:04:43 +0300

    Homework. Task 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,7 +4,15 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
 
-    System.out.println("Hello, world!");
+    System.out.println("Hello, Alexei!");
+
+    Point p1 = new Point(9, -1);
+    Point p2 = new Point(2, -25);
+
+    System.out.println("Расстояние между двумя точками "
+            + "(" + p1.x + "," + p1.y + ")"
+            + " и " + "(" + p2.x + "," + p2.y + ")"
+            + " = " + p1.distance(p2));
   }
 
 }
